b2432a1410e65c9cadb2eab3389545ff1e91ba37
Hao Hou <haohou302@gmail.com>
2019-09-13 09:48:47 -0600

    Add depth iter

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,82 @@
 pub mod alignment;
 
+pub use alignment::*;
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+pub struct DepthIter<'a, R: AlignmentReader<'a>> {
+    iter: AlignmentIter<'a, R>,
+    cur_chrom: i32,
+    cur_pos: usize,
+    heap: BinaryHeap<Reverse<usize>>,
+    next_read: Option<(i32, usize, usize)>,
+}
+
+impl<'a, R: AlignmentReader<'a>> DepthIter<'a, R> {
+    pub fn new(reader: R) -> Self {
+        let mut iter = reader.to_alignment_iter();
+        let next_read = iter.next().map(|x| {
+            (
+                x.as_ref().unwrap().ref_id(),
+                x.as_ref().unwrap().ref_begin(),
+                x.as_ref().unwrap().ref_end(),
+            )
+        });
+        Self {
+            iter,
+            next_read,
+            cur_chrom: 0,
+            cur_pos: 0,
+            heap: BinaryHeap::new(),
+        }
+    }
+}
+
+impl<'a, R: AlignmentReader<'a>> Iterator for DepthIter<'a, R> {
+    type Item = (i32, usize, u32);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_read.is_none() && self.heap.len() == 0 {
+            return None;
+        }
+
+        let ret = (self.cur_chrom, self.cur_pos, self.heap.len() as u32);
+
+        self.cur_pos += 1;
+
+        if self.cur_pos % 1000_0000 == 0 {
+            eprintln!("{} {}", self.cur_chrom, self.cur_pos);
+        }
+
+        while let Some((tid, left, right)) = self.next_read {
+            if tid != self.cur_chrom {
+                if self.heap.len() == 0 {
+                    self.cur_chrom = tid;
+                    self.cur_pos = 0;
+                }
+                break;
+            }
+            if left > self.cur_pos {
+                break;
+            }
+            self.heap.push(Reverse(right));
+            self.next_read = self.iter.next().map(|x| {
+                (
+                    x.as_ref().unwrap().ref_id(),
+                    x.as_ref().unwrap().ref_begin(),
+                    x.as_ref().unwrap().ref_end(),
+                )
+            });
+        }
+
+        while self.heap.peek().map_or(false, |x| x.0 < self.cur_pos) {
+            self.heap.pop();
+        }
+
+        return Some(ret);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
